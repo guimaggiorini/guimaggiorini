a93f992e519346c5df7b5e6e0ce63edfe9e28db2
Guilherme <69226067+guihm1@users.noreply.github.com>
2023-06-08 17:16:18 -0300

    Update readme.js

--- readme.js
+++ readme.js
@@ -4,3 +4,10 @@ const learning = "🌱 Currently learning JavaScript"
 const highschool = "📚 Finishing High School"
 const lastproject = "🔗 Last project: https://live2you.com.br"
 const contact = "📫 How to reach me: guimaggiorini@icloud.com"
+
+console.log(hi)
+console.log(interested)
+console.log(learning)
+console.log(highschool)
+console.log(lastproject)
+console.log(contact)
